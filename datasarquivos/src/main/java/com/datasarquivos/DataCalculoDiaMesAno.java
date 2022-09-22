15b191422b1b58431f990a808675f33a94988381
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-22 08:33:38 -0300

    Calculo Data em String

--- datasarquivos/src/main/java/com/datasarquivos/DataCalculoDiaMesAno.java
+++ datasarquivos/src/main/java/com/datasarquivos/DataCalculoDiaMesAno.java
@@ -3,7 +3,7 @@ package com.datasarquivos;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.Locale;  
 
 public class DataCalculoDiaMesAno {
     public static void main(String[] args) throws ParseException {
@@ -15,9 +15,19 @@ public class DataCalculoDiaMesAno {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy",Locale.getDefault());/* Data qualquer */
         Calendar calendar = Calendar.getInstance();/* pega a data atual */
         calendar.setTime(sdf.parse(data));
-        calendar.add(Calendar.DAY_OF_MONTH, 5);
+        calendar.add(Calendar.DAY_OF_MONTH, -40);
 
-        System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+        System.out.println("Data: "+data);
+        System.out.println("------");
+        System.out.println("Data menos 40 dias: "+new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+
+        calendar.add(Calendar.MONTH, 1);
+        System.out.println("------");
+        System.out.println("Somando o mes: "+new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+
+        calendar.add(Calendar.YEAR, 1);
+        System.out.println("------");
+        System.out.println("Somando 1 ano: "+new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
 
     }
 }
