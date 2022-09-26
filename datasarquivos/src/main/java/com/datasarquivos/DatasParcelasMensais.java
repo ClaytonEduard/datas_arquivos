7583b9ccec7e8cb126786f04d227b5dd92e85b89
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-26 06:26:36 -0300

    Parcelamento com data

---
+++ datasarquivos/src/main/java/com/datasarquivos/DatasParcelasMensais.java
@@ -0,0 +1,27 @@
+package com.datasarquivos;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatasParcelasMensais {
+
+    public static void main(String[] args) throws ParseException {
+
+        Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("26/09/2022");/* Data inicial */
+
+        Calendar cal = Calendar.getInstance(); /* instancio o Calendar */
+
+        cal.setTime(dataInicial); /* passa a data informada */
+
+        for (int i = 1; i <= 12; i++) {
+            cal.add(Calendar.MONTH, 1); /* adciona mais um mes de 30 dias */
+            System.out.println("--------------------------------");
+            System.out.println("Parcela numero: " + i + " vencimento e em: "
+                    + new SimpleDateFormat("dd/MM/yyyy").format(cal.getTime()));
+        }
+
+    }
+
+}
