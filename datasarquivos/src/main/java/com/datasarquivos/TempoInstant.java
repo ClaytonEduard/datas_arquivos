2230e2a5791a0b8416a564b415addd99bda18c28
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-26 07:15:33 -0300

    Pacote Instant

---
+++ datasarquivos/src/main/java/com/datasarquivos/TempoInstant.java
@@ -0,0 +1,22 @@
+package com.datasarquivos;
+
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.Instant;
+
+public class TempoInstant {
+    public static void main(String[] args) throws ParseException, InterruptedException {
+        Instant start = Instant.now();
+        Thread.sleep(2000);/* pode ser um processo com tem po qualquer */
+
+        Instant out = Instant.now();
+
+        Duration duration = Duration.between(start, out);
+        System.out.println("Duracao em nano segundo: "+duration.toNanos());
+        System.out.println("Duracao em minutos: "+duration.toMinutes());
+        System.out.println("Duracao em horas: "+duration.toHours());
+        System.out.println("Duracao em milis segundo: "+duration.toMillis());
+        System.out.println("Duracao em dias: "+duration.toDays());
+
+    }
+}
